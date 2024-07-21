51495bbcb9d8055e0b1b71d563cc55248526507e
Namitha <namitha.chandran@gemsessence.com>
2024-07-21 19:43:52 +0530

    Added changes changes for two numbers cases.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,8 @@ class StringCalculator
   def add(str_numbers)
     return 0 if str_numbers.empty?
     return str_numbers.to_i if str_numbers.length == 1
+
+    numbers = str_numbers.split(',').map(&:to_i)
+    numbers.sum
   end
 end
